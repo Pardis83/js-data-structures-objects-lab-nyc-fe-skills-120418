2ae7a7f774e81068bec88c13ae2fd2fd9a37cf9e
Pardis Rezaei <pardisrezaei@gmail.com>
2019-01-25 00:31:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const driver={}
   copyDriver[key] = value
   return copyDriver
 }
-function function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  driver [key]=value
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver [key] = value
   return driver
 }
